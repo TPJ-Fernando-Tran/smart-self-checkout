926f740c4d93a573545de41137bd5e74217b45ed
Daniel Tran <baophuc247@gmail.com>
2024-10-22 10:56:17 -0400

    done troubleshooting backend_url

--- src/components/EnvDebug.js
+++
@@ -1,9 +0,0 @@
-const EnvDebug = () => {
-  return (
-    <div className="fixed bottom-0 right-0 bg-black text-white p-2 text-xs">
-      Backend URL: {process.env.NEXT_PUBLIC_BACKEND_URL || "not set"}
-    </div>
-  );
-};
-
-export default EnvDebug;
